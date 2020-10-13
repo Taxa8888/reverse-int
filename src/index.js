ed93dfd9f991ffa7f7954ed0d7d7cc0ff2a8acd7
Taxa8888 <Taxa8888@bk.ru>
2020-10-13 23:12:40 +0300

    feat: add resolution of issue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+    if ( n >= 0) {
+        return parseFloat(n.toString().split('').reverse().join(''));
+    } else {
+        return parseFloat(Math.abs(n).toString().split('').reverse().join(''))
+    }
   
 }
